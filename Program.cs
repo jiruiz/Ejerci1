dd04db4a12bfacc6b4c1e57850118ab079effba2
Juan Ruiz <juanignacioruiz540@gmail.com>
2020-08-29 16:57:56 -0300

    hagregamos el cogigo csharp

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("hola buenas noches");
+            
+            //variable
+            string name = "Juani";
+            name = "pepe";
+            Console.WriteLine(name);
+            
+            
+            int numberA = 1;
+            int numberB = 5;
+            int result = numberA + numberB;
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
